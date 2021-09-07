359f0c11a65c97e8b49d39431dda5da05022b319
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 09:29:04 +0530

    Add project files.

---
+++ Finalize/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FinalizeMethod
+{
+    class ExClass
+    {
+        Stopwatch sw;
+
+        public ExClass()
+        {
+            sw = Stopwatch.StartNew();
+            Console.WriteLine("Instantiated object");
+        }
+
+        public void ShowDuration()
+        {
+            Console.WriteLine("This instance of {0} has been in existence for {1}",
+                              this, sw.Elapsed);
+        }
+
+        ~ExClass()
+        {
+            Console.WriteLine("Finalizing object");
+            sw.Stop();
+            Console.WriteLine("This instance of {0} has been in existence for {1}",
+                              this, sw.Elapsed);
+        }
+    }
+
+    public class Demo
+    {
+        public static void Main()
+        {
+            ExClass ex = new ExClass();
+            ex.ShowDuration();
+        }
+    }
+}
